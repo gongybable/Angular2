d07750b90bc76e075081972bba390a401aa40cb9
Yibo Gong <yibo.gong@indexexchange.com>
2016-07-04 20:42:47 -0400

    add first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,43 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/common';
+
+import { LoginInfo } from './loginInfo';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>My First Angular 2 App</h1>`
+  template: `
+      <h1>My First Angular 2 App</h1>
+      <form class="form-horizontal">
+        <div class="form-group">
+            <label class="control-label col-xs-12 col-sm-3 col-lg-2">Email Address:</label>
+            <div class="col-xs-12 col-sm-4 col-lg-3">
+                <input type="text" class="form-control" [(ngModel)]="loginInfo.emailAddress" name="emailAddress">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-xs-12 col-sm-3 col-lg-2">Password:</label>
+            <div class="col-xs-12 col-sm-4 col-lg-3">
+                <input type="text" class="form-control" [(ngModel)]="loginInfo.password" name="password">
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="col-xs-12 col-sm-3 col-lg-2 control-label">&nbsp;</label>
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+                <button type="button" class="btn btn-primary" (click)="logIn()">Login</button>
+            </div>
+        </div>
+    </form>
+    `
 })
 
-export class AppComponent { }
+export class AppComponent {
+    loginInfo: LoginInfo = {
+        emailAddress: '',
+        password: ''
+    };
+
+    logIn() {
+        console.log("Login button clicked with login infomation:" + JSON.stringify(this.loginInfo));
+    }
+}
---
+++ app/loginInfo.ts
@@ -0,0 +1,4 @@
+export class LoginInfo {
+    emailAddress: string;
+    password: string;
+}
