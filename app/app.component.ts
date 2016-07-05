01b8c5c53eebcc014ca2d15dd4f7b84cba9e4c4d
Yibo Gong <yibo.gong@indexexchange.com>
2016-07-05 12:32:23 -0400

    First Commit for Tutorial2

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,30 +5,7 @@ import { LoginInfo } from './loginInfo';
 
 @Component({
   selector: 'my-app',
-  template: `
-      <h1>My First Angular 2 App</h1>
-      <form class="form-horizontal">
-        <div class="form-group">
-            <label class="control-label col-xs-12 col-sm-3 col-lg-2">Email Address:</label>
-            <div class="col-xs-12 col-sm-4 col-lg-3">
-                <input type="text" class="form-control" [(ngModel)]="loginInfo.emailAddress" name="emailAddress">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="control-label col-xs-12 col-sm-3 col-lg-2">Password:</label>
-            <div class="col-xs-12 col-sm-4 col-lg-3">
-                <input type="text" class="form-control" [(ngModel)]="loginInfo.password" name="password">
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-xs-12 col-sm-3 col-lg-2 control-label">&nbsp;</label>
-            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
-                <button type="button" class="btn btn-primary" (click)="logIn()">Login</button>
-            </div>
-        </div>
-    </form>
-    `
+  template: 'app/app.component.html'
 })
 
 export class AppComponent {
