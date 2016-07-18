5d5e665367739c7486f9e7e0aaf2647ecfe0e078
Yibo Gong <yibo.gong@indexexchange.com>
2016-07-18 12:30:30 -0400

    fix constant issue

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -3,8 +3,6 @@ import { NgForm } from '@angular/common';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { Router } from '@angular/router';
 
-import { Observable } from 'rxjs/Rx';
-
 import { AuthInfo } from '../shared/auth-info';
 import { AuthService } from '../shared/nav/auth.service';
 import { CONSTANTS } from '../shared/constant';
@@ -16,6 +14,8 @@ import { CONSTANTS } from '../shared/constant';
 })
 
 export class LoginComponent {
+    mode = 'Observable';
+
     constructor(
         private authService: AuthService,
         private router: Router
--- app/shared/constant.ts
+++ app/shared/constant.ts
@@ -6,7 +6,7 @@ export const CONSTANTS: {
 	baseApiUrl: string;
 	httpOptions: HttpOptions;
 } = {
-	baseApiUrl: 'http://192.168.1.130:8000/api',
+	baseApiUrl: 'http://localhost:8000/api',
 	httpOptions: {
 		withCredentials: true
 	}
